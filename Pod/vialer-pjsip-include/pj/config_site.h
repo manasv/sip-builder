00932f9d110d2ded0e6b9a285de33073e0a8b2f6
Manuel Sanchez <manasan93@gmail.com>
2022-03-03 15:20:59 +0000

    Updated PJSIP

---
+++ Pod/vialer-pjsip-include/pj/config_site.h
@@ -0,0 +1,6 @@
+#define PJ_CONFIG_IPHONE 1
+#define PJMEDIA_SDP_NEG_ANSWER_SYMMETRIC_PT 1
+#define PJSIP_AUTH_HEADER_CACHING 1
+#define PJSIP_AUTH_AUTO_SEND_NEXT 1
+#define PJMEDIA_HAS_OPUS_CODEC 1
+#include <pj/config_site_sample.h>
